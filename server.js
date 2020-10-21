dc7d92bd62ffa68e3db22fa94e73719011e49e5d
Kylie S <kyliemegan24@gmail.com>
2020-10-21 16:11:05 -0500

    routed html files

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const app = express();
+const path = require('path');
 
 let PORT = process.env.PORT || 3000;
 
@@ -8,6 +9,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); 
 app.use( express.static('public'));
 
+//HTML Routes
+app.get('/notes', function(req, res) {
+    res.sendFile(path.join(__dirname, "../notetaker/public/notes.html"))
+});
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"))
+});
+
 app.listen(PORT, function() {
     console.log("App listening on PORT", PORT);
-})
+});
